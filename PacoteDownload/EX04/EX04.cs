10807b994ed8e677b2bafce3d0b89f1350482bb9
Paulo Santos <86374948+nwcoala@users.noreply.github.com>
2021-09-24 00:08:49 -0300

    commit EX04

---
+++ PacoteDownload/EX04/EX04.cs
@@ -0,0 +1,22 @@
+using System;
+class EX04{
+    static void Main(){
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine("     Calculo qtd Litros de gasolina        ");
+        Console.WriteLine("-------------------------------------------");
+
+        double preco,valor,total;
+
+        Console.WriteLine("Digite o preço da Gasolina: ");
+        preco = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o Valor que deseja abastecer: ");
+        valor = Convert.ToDouble(Console.ReadLine());
+
+        total = valor / preco;
+        
+        Console.WriteLine("Quantidade Abastecida: {0} Lts",total.ToString("F"));
+        
+
+    }
+}
